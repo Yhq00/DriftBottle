e87edd83bc763a1fd9a759f09e3e6c73cf5fda23
Yhq <1193086708@qq.com>
2023-05-03 14:21:10 +0800

    feat:漂流瓶输入界面

--- src/views/send/index.tsx
+++ src/views/send/index.tsx
@@ -1,9 +1,70 @@
-import React, { Component } from "react";
+import React from "react";
+import styles from "./index.module.scss";
+import { Menu, MenuProps, Input } from "antd";
+import { useNavigate, To } from "react-router-dom";
+import throwBottle from "../../assets/images/throw.png";
+const Send = () => {
+  type MenuItem = Required<MenuProps>["items"][number];
 
-class Send extends Component {
-  render() {
-    return <div>Send</div>;
+  function getItem(
+    label: React.ReactNode,
+    key?: React.Key | null,
+    icon?: string,
+    children?: MenuItem[],
+    type?: "group"
+  ): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      type,
+    } as MenuItem;
   }
-}
+  const items = [
+    getItem(
+      <img
+        src={throwBottle}
+        style={{
+          width: "5rem",
+          height: "5rem",
+          paddingTop: "1rem",
+        }}
+        alt="throw bottle"
+      />,
+      "/Send"
+    ),
+  ];
+  const navigate = useNavigate();
+  const onClick = (e: { key: To }) => {
+    // console.log("click ", e);
+    navigate(e.key);
+  };
+  const { TextArea } = Input;
+  return (
+    <>
+      <div>
+        <div className={styles.body}>
+          <TextArea
+            style={{
+              width: "80%",
+              height: "70%",
+              margin: "5rem 0rem 6rem 10rem",
+            }}
+            placeholder="你可以在这畅所欲言"
+          ></TextArea>
+        </div>
+        <div className={styles.footer}>
+          <Menu
+            mode="horizontal"
+            className={styles.menu}
+            onClick={onClick}
+            items={items}
+          ></Menu>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Send;
