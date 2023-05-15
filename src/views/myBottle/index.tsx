2e3cef333f9de3966d9296422dbc24dc910da0ee
Yhq <1193086708@qq.com>
2023-05-15 19:27:01 +0800

    feat:前端界面编写

--- src/views/myBottle/index.tsx
+++ src/views/myBottle/index.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./index.module.scss";
 import { useLocation } from "react-router-dom";
-import { Empty } from "antd";
+import { Empty, Card } from "antd";
 const MyBottle = () => {
   const location = useLocation();
   const userId = location.state?.userId;
+  useEffect(() => {}, []);
   return (
     <>
       {typeof userId !== "undefined" ? (
         <div className={styles.body}>
           <div className={styles.header}>我的瓶子</div>
+          <div>
+            <Card></Card>
+          </div>
         </div>
       ) : (
         <Empty description={<span>非法跳转！！</span>}></Empty>
