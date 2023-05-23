cb93c875d6f623f0efd0b29249e1641e4b53dca3
Yhq <1193086708@qq.com>
2023-05-23 20:30:29 +0800

    feat 前端页面编写

--- src/views/myBottle/index.tsx
+++ src/views/myBottle/index.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styles from "./index.module.scss";
 import { useLocation } from "react-router-dom";
-import { Empty, Card, message } from "antd";
+import { Empty, Card, message, Tooltip } from "antd";
 import { getMyBottles } from "../../service/text";
+import Bottle from "../../assets/images/bottle.png";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 const MyBottle = () => {
   const location = useLocation();
   const userId = location.state?.userId;
   const [textList, setTextList] = useState<any>([]);
   useEffect(() => {
     getMyBottles(userId.userId).then((res) => {
-      console.log(res.data);
+      // console.log(res.data);
       if (res.status === 200) {
         if (res.data.code === 200) {
           if (res.data.data.length !== 0) {
@@ -24,13 +26,33 @@ const MyBottle = () => {
         message.error("请求失败！");
       }
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const lookDetails = (textId: string) => {
+    console.log(textId);
+  };
+
   const listCard = () => {
     return textList.map((item: any) => {
       return (
-        <>
-          <Card>{item.textContent}</Card>
-        </>
+        <Card
+          key={item.textId}
+          style={{ width: 300, margin: "5px 0px 10px 20px" }}
+          cover={<img alt="bottle" src={Bottle} style={{ height: "250px" }} />}
+          actions={[
+            <Tooltip title="查看详情">
+              <EditOutlined
+                key="edit"
+                onClick={() => lookDetails(item.textId)}
+              />
+            </Tooltip>,
+            <Tooltip title="打碎瓶子">
+              <DeleteOutlined key="del" />
+            </Tooltip>,
+          ]}
+        >
+          {item.textContent}
+        </Card>
       );
     });
   };
@@ -39,7 +61,7 @@ const MyBottle = () => {
       {typeof userId !== "undefined" ? (
         <div className={styles.body}>
           <div className={styles.header}>我的瓶子</div>
-          {listCard()}
+          <div className={styles.cardBody}>{listCard()}</div>
         </div>
       ) : (
         <Empty description={<span>非法跳转！！</span>}></Empty>
