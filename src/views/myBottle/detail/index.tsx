4a8bf4a917b30d3031a43b765cfded9466bdfe51
Yhq <1193086708@qq.com>
2023-05-24 14:58:40 +0800

    feat 前端页面编写

--- src/views/myBottle/detail/index.tsx
+++ src/views/myBottle/detail/index.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { getDetail } from "../../../service/text";
-// import {}
+import styles from "./index.module.scss";
+import { Empty } from "antd";
+import TextArea from "antd/es/input/TextArea";
 const Detail = () => {
   // const navigate = useNavigate();
   const location = useLocation();
@@ -10,7 +12,30 @@ const Detail = () => {
   useEffect(() => {
     getDetail(textId);
   }, []);
-  return <>{textContent}</>;
+  return (
+    <>
+      {typeof textId !== "undefined" ? (
+        <div className={styles.body}>
+          <div className={styles.title}>
+            <div>我的瓶子:</div>
+            <TextArea
+              style={{
+                height: "400px",
+                // margin: "5px 0px 10px 180px",
+                color: "black",
+                fontSize: "20px",
+                textIndent: "2ch",
+              }}
+              value={textContent}
+              disabled={true}
+            ></TextArea>
+          </div>
+        </div>
+      ) : (
+        <Empty description={<span>非法跳转！！</span>}></Empty>
+      )}
+    </>
+  );
 };
 
 export default Detail;
