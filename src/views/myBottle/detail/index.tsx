f3c0fa0adf5cddb10e4776517f8aec5397323302
Yhq <1193086708@qq.com>
2023-05-24 15:19:04 +0800

    feat 前端页面编写

--- src/views/myBottle/detail/index.tsx
+++ src/views/myBottle/detail/index.tsx
@@ -1,17 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getDetail } from "../../../service/text";
 import styles from "./index.module.scss";
-import { Empty } from "antd";
+import { Empty, message } from "antd";
 import TextArea from "antd/es/input/TextArea";
 const Detail = () => {
   // const navigate = useNavigate();
   const location = useLocation();
   const textId = location.state?.textId;
   const textContent = location.state?.textContent;
+  const [replyList, setReplyList] = useState<any>([]);
   useEffect(() => {
-    getDetail(textId);
+    getDetail(textId).then((res) => {
+      // console.log(res.data.);
+      if (res.status === 200) {
+        if (res.data.data.length > 0) {
+          setReplyList(res.data.data);
+        }
+      } else {
+        message.error("请求失败！");
+      }
+    });
   }, []);
+  const replyComponents = () => {
+    return replyList.map((res: any) => (
+      <div className={styles.commentCard}>
+        <div className={styles.name}>{res.userNickname}回复说&nbsp;:</div>
+        <div className={styles.reply}>{res.replyContent}</div>
+      </div>
+    ));
+  };
   return (
     <>
       {typeof textId !== "undefined" ? (
@@ -29,6 +47,16 @@ const Detail = () => {
               value={textContent}
               disabled={true}
             ></TextArea>
+          </div>
+          <div>
+            <div>
+              <h3>回复</h3>
+            </div>
+            {replyComponents().length > 0 ? (
+              replyComponents()
+            ) : (
+              <Empty description={<span>暂无回复</span>}></Empty>
+            )}
           </div>
         </div>
       ) : (
